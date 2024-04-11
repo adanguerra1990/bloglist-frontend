38d17630bc8dafd5c7e20bd36a4fa8844371eca7
adanguerra1990 <aj90guerra@gmail.com>
2024-04-11 15:46:33 -0400

    Implementando inicio de sesión "permanente" mediante el use de local storage

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,14 @@ const App = () => {
     )
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, []);
+
   const handdleLogin = async (event) => {
     event.preventDefault()
 
@@ -22,6 +30,9 @@ const App = () => {
       const user = await loginService.login({
         username, password
       })
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
       setUser(user)
       setUsername('')
       setPassword('')
