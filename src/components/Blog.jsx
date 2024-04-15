fb29527fd6720a2ffa348ce11b4c23a4b309646a
adanguerra1990 <aj90guerra@gmail.com>
2024-04-15 15:37:06 -0400

    Implementado boton para mostrar y ocultar los detalles del blog

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,45 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog, }) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleDetails = () => {
+    setShowDetails(!showDetails)
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title}
+        <button onClick={toggleDetails}>
+          {showDetails ? 'hide' : 'view'}
+        </button>
+      </div>
+      {showDetails && (
+        <div>
+          <div>
+            {blog.author}
+          </div>
+          <div>
+            {blog.url}
+          </div>
+          <div>
+            likes: {blog.likes}
+            <button>like</button>
+          </div>
+        </div>
+      )}
+
+    </div>
+  )
+}
 
 export default Blog
