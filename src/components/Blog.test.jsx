04d776029ac1d90c0dba643e5db29ee333f6b729
adanguerra1990 <aj90guerra@gmail.com>
2024-04-25 11:22:04 -0400

    Muestra y oculta la URL y los likes del blog cuando se hace clic en el botón de view

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -1,22 +1,25 @@
-import { render, screen } from "@testing-library/react"
+import { render, screen, waitFor } from "@testing-library/react"
 import Blog from "./Blog"
+import userEvent from "@testing-library/user-event";
 
 describe('<Blog />', () => {
-    test('Muestra el titulo y el author del blog, pero no la url y los likes de forma predeterminada', () => {
-        const blog = {
-            title: 'Titulo del Libro',
-            author: 'Fabricio',
-            url: 'www.urlprueba.com',
-            likes: 2,
-            user: {
-                name: 'User test',
-                id: 1
-            }
-        }
 
-        // Mock de funciones
-        const updateLikes = vi.fn()
-        const onDelete = vi.fn()
+    const blog = {
+        title: 'Titulo del Libro',
+        author: 'Fabricio',
+        url: 'www.urlprueba.com',
+        likes: 2,
+        user: {
+            name: 'User test',
+            id: 1
+        }
+    }
+
+    // Mock de funciones
+    const updateLikes = vi.fn()
+    const onDelete = vi.fn()
+
+    test('Muestra el titulo y el author del blog, pero no la url y los likes de forma predeterminada', () => {
 
         // Mock del servicio de blogs
         vi.mock('../services/blogs', () => ({
@@ -33,11 +36,38 @@ describe('<Blog />', () => {
         const blogContainer = container.querySelector('.container-blog')
 
         // Verificación de la presencia del título y el autor
-        expect(blogContainer).toHaveTextContent('Titulo del Libro')
-        expect(blogContainer).toHaveTextContent('Fabricio')
+        expect(blogContainer).toHaveTextContent(blog.title)
+        expect(blogContainer).toHaveTextContent(blog.author)
 
         // Verificación de la ausencia de la URL y los likes
-        expect(blogContainer).not.toHaveTextContent('www.urlprueba.com')
-        expect(blogContainer).not.toHaveTextContent('likes: 2')
+        expect(blogContainer).not.toHaveTextContent(blog.url)
+        expect(blogContainer).not.toHaveTextContent(`likes: ${blog.likes}`)
+    })
+
+    test('Muestra la URL y los likes del blog cuando se hace clic en el botón de view', async () => {
+        render(
+            <Blog blog={blog} updateLikes={updateLikes} onDelete={onDelete} currentUserId='1' />
+        )
+        // Verifica que el botón para mostrar detalles está presente y hace clic en él
+        const detailsButton = screen.getByText('view')
+        userEvent.click(detailsButton)
+
+        // Verifica que la URL del blog y el número de likes se muestran
+        const blogUrl = await screen.findByText(blog.url)
+        const blogLikes = await screen.findByText(`likes: ${blog.likes}`)
+
+        expect(blogUrl).toBeInTheDocument()
+        expect(blogLikes).toBeInTheDocument()
+
+        expect(detailsButton).toHaveTextContent('hide')
+        userEvent.click(detailsButton)
+
+        // Espera a que el DOM se actualice antes de realizar las aserciones
+        await waitFor(() => {
+            expect(screen.queryByText(blog.url)).not.toBeInTheDocument();
+            expect(screen.queryByText(`likes: ${blog.likes}`)).not.toBeInTheDocument()
+        })
+
+        expect(detailsButton).toHaveTextContent('view')
     })
 });
